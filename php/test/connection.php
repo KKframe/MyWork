5d00833c63556f5f51e138bf9d793d2527e20e3b
Kanyaphat_Khiewsang <82822586+KKframe@users.noreply.github.com>
2022-05-03 11:07:03 +0700

    Delete php/test directory

--- php/test/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="localhost";
-$usr="db620710038";
-$pwd="D-3080b";
-$db = "std_u620710038";
-$con=mysqli_connect($host,$usr,$pwd,$db) or
-die("Sorry... Something was wrong for you.");
-
-?>
